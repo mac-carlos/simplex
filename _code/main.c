3615dca494c54fe97193e79c3a0f218f06f0eefa
mac-carlos <mat.carlosqueiroz@gmail.com>
2022-04-02 21:18:19 -0300

    Initial commit

--- _code/main.c
+++ _code/main.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
-#include <assert.h>
-#include <malloc.h>
+#include <stdbool.h>
+
 #include "simplex.h"
 
-int main(void) {
-    MATRIX *a;
+int main(){
+    int vars, rest; // Numero de variaveis e restricoes
 
-    a = create(2, 2);
+	vars = 2;
+    rest = 2;
+    float z[2] = {3, 2};
+    float a[2][2] = {{1, 1}, {5, 2}};
+    float b[2] = {6, 20};
 
-    entry(a, 1, 1, 1.0);
-    entry(a, 1, 2, 2.0);
-    entry(a, 2, 1, 3.0);
-    entry(a, 2, 2, 4.0);
+    /*
+	vars = 3;
+    rest = 4;
 
-    display(a);
+    float z[3] = {10, 20, 30};
+    float a[4][3] = {{2, 2, 4}, {0, 4, 3}, {1, 0, 0}, {4, 3, 0}};
+    float b[4] = {300, 200, 20, 50};
+    */
 
-    delete(a);
+	float maximo = 0;
+
+    printf(" Tabela inicial: ");
+
+    mostrar_tabela (rest, vars, a, z, b);
+
+    simplex( rest, vars, z, a, b, maximo );
 }
+
--- _code/simplex.h
+++ _code/simplex.h
@@ -1,44 +1,189 @@
-typedef struct {
-    int rows;
-    int cols;
-    double *value;
-} MATRIX;
+#ifndef SIMPLEX_H_INCLUDED
+#define SIMPLEX_H_INCLUDED
 
-MATRIX *create(int rows, int cols) {
-    if(rows <= 0 || cols <= 0) 
-        return NULL;
-    
-    MATRIX *matrix = (MATRIX *)malloc(sizeof(MATRIX));
 
-    matrix->rows = rows;
-    matrix->cols = cols;
+void mostrar_tabela( int rest, int vars, float a[rest][vars], float z[vars], float b[rest] )
+{
+    int i, j;
 
-    matrix->value = (double *)malloc(rows*cols*sizeof(double));
+    printf(" \n\n Z: \t");
 
-    for(int i = 0; i < rows*cols; i++)
-        matrix->value[i] = 0.0;
+    for (i = 0; i < vars; i++)
+    {
+        printf("%.2f\t", z[i]);
+    }
 
-    return matrix;
-}
+    for (i = 0; i < rest; i++)
+    {
+        printf("\n F%d \t", i);
 
-#define ELEMENT(matrix, row, col)\
-        matrix->value[(col-1)*matrix->rows+(row-1)]
+        for (j = 0; j < vars; j++)
+        {
+            printf("%.2f \t", a[i][j]);
+        }
 
-void entry(MATRIX *matrix, int row, int col, double value) {  
-    assert(matrix->value);
-    ELEMENT(matrix, row, col) = value;
-}
-
-void display(MATRIX *matrix) {
-    for(int i = 1; i <= matrix->rows; i++) {
-        for(int j = 1; j <= matrix->cols; j++)
-            printf("% 6.2f ", ELEMENT(matrix, i, j));
-        printf("\n");
+        printf("| %.2f", b[i]);
     }
 }
 
-void delete(MATRIX *matrix) {
-    assert(matrix->value);
-    free(matrix->value);
-    free(matrix);
+
+void simplex(int rest, int vars, float z[vars], float a[rest][vars], float b[rest], float maximo)
+{
+    int i, j, temp;
+
+
+    // Adicionando as variaveis de folga
+    temp = vars + rest; // Novo numero de variaveis
+
+    float a_folgada[rest][temp], z_folgada[temp];
+
+    // Folga da matriz A
+    for (i = 0; i < rest; i++)
+    {
+        for (j = 0; j < temp; j++)
+        {
+            if (j <= (vars - 1))    // Copia a matriz inicial para a nova matriz
+            {
+                a_folgada[i][j] = a[i][j];
+            }
+
+            else if (j == (vars + i))   // Adiciona 1 para os valores diagonais
+            {
+                a_folgada[i][j] = 1;
+            }
+
+            else    // Adiciona 0 para os valores nao diagonais
+            {
+                a_folgada[i][j] = 0;
+            }
+        }
+    }
+
+    // Folga dos valores de Z (funcao objetivo)
+    for (i = 0; i < temp; i++)
+    {
+        if(i < vars)
+        {
+            z_folgada[i] = -z[i];
+        }
+
+        else
+        {
+            z_folgada[i] = 0;
+        }
+    }
+
+    printf ("\n\n\n Tabela com as variaveis de folga e z negativo: ");
+
+    mostrar_tabela (rest, temp, a_folgada, z_folgada, b);
+
+
+
+    float pivo, coeficiente;
+    bool parar = false;
+
+    do
+    {
+        // Encontrar a coluna pivo
+        float menor;
+        int col_pivo;
+
+        menor = z_folgada[0];
+        col_pivo = 0;
+
+        for (i = 1; i < temp; i++)
+        {
+            if (menor > z_folgada[i])
+            {
+                menor = z_folgada[i];
+                col_pivo = i;
+            }
+        }
+
+
+        // Encontrar a linha pivo
+        int lin_pivo;
+
+        menor = b[0] / a_folgada[0][col_pivo];
+        lin_pivo = 0;
+
+        for (i = 1; i < rest; i++)
+        {
+            if ( (menor > b[i] / a_folgada[i][col_pivo]) && (a_folgada[i][col_pivo] != 0) )
+            {
+                menor = b[i] / a_folgada[i][col_pivo];
+                lin_pivo = i;
+            }
+        }
+
+
+        // O pivo
+        pivo = a_folgada[lin_pivo][col_pivo];
+
+
+        // Mostrar as informacoes
+        printf("\n\n * Pivo: %.2f [linha: %d, coluna: %d] \n",
+               a_folgada[lin_pivo][col_pivo], lin_pivo + 2, col_pivo + 1);
+        printf(" * Maximo: %.2f \n\n", maximo);
+
+
+        // Escalonamento
+        for (i = 0; i < temp; i++)
+        {
+            a_folgada[lin_pivo][i] = a_folgada[lin_pivo][i] / pivo;
+        }
+
+        b[lin_pivo] = b[lin_pivo] / pivo;
+
+        for (i = 0; i < rest; i++)
+        {
+            coeficiente = a_folgada[i][col_pivo];   // Coeficiente que esta na coluna pivo
+
+            if (i != lin_pivo)
+            {
+                for (j = 0; j < temp; j++)
+                {
+                    a_folgada[i][j] = a_folgada[i][j] - (coeficiente * a_folgada[lin_pivo][j]); // Zerando a coluna do pivo
+                }
+
+                b[i] = b[i] - coeficiente * b[lin_pivo];
+            }
+        }
+
+        coeficiente = z_folgada[col_pivo];
+
+        for(i = 0; i < temp; i++)
+        {
+            z_folgada[i] -= (coeficiente * a_folgada[lin_pivo][i]);
+        }
+
+        maximo = maximo - (coeficiente * b[lin_pivo]);
+
+
+        // Mostrar a tabela escalonada
+        mostrar_tabela (rest, temp, a_folgada, z_folgada, b);
+
+
+        // Criterio de parada
+        menor = 0;
+
+        for (i = 0; i < temp; i++)   // verificacao de otimalidade
+        {
+            if (z_folgada[i] >= 0)
+            {
+                menor++;
+            }
+        }
+
+        if (menor == temp)
+        {
+            parar = true;
+        }
+    }
+    while (parar == false);
+
+    printf("\n\n - Maximo: %.2f \n", maximo);
 }
+
+
+#endif // SIMPLEX_H_INCLUDED
